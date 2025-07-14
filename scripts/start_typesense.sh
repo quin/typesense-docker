7771a4ee4990a233deeb1e8f8944aec5fd60d89f
Quincy <q.babin@gmail.com>
2025-07-14 15:48:36 -0700

    bash

--- scripts/start_typesense.sh
+++ scripts/start_typesense.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 exec /opt/typesense-server --api-address 127.0.0.1 --api-port 8118
+exec /opt/typesense-server \
+  --api-address 127.0.0.1 \
+  --api-port 8118 \
+  --data-dir /data \
+  --api-key "${TYPESENSE_API_KEY}" \
+  --enable-cors
